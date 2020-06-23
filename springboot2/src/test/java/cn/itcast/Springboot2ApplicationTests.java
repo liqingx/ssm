917e70f953e407e15138ec9563f562f096ea4fbb
lqx <1830865198@qq.com>
2020-06-23 20:54:42 +0800

    本地工程添加到git

---
+++ springboot2/src/test/java/cn/itcast/Springboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
